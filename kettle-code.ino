cf5e4ced0a2469809ba0a890d3f285614813d50a
cmmdrdata <ifconfig777@yahoo.com>
2020-02-10 14:49:02 -0600

    initial commit

---
+++ kettle-code.ino
@@ -0,0 +1,222 @@
+/*
+ Version 0.4 - April 26 2019
+*/
+
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WebSocketsClient.h> //  https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries
+#include <ArduinoJson.h> // https://github.com/kakopappa/sinric/wiki/How-to-add-dependency-libraries (use the correct version)
+#include <StreamString.h>
+
+#include <Servo.h>
+
+#define FILL_DELAY 10 //seconds to fill kettle
+Servo myservo;  // create servo object to control a servo
+
+ESP8266WiFiMulti WiFiMulti;
+WebSocketsClient webSocket;
+WiFiClient client;
+
+#define MyApiKey "from sinric" // TODO: Change to your sinric API Key. Your API Key is displayed on sinric.com dashboard
+#define MySSID "" // TODO: Change to your Wifi network SSID
+#define MyWifiPassword "*****" // TODO: Change to your Wifi network password
+
+#define HEARTBEAT_INTERVAL 300000 // 5 Minutes
+#define FILL 5
+#define ON HIGH
+#define OFF LOW
+
+uint64_t heartbeatTimestamp = 0;
+bool isConnected = false;
+
+
+// deviceId is the ID assgined to your smart-home-device in sinric.com dashboard. Copy it from dashboard and paste it here
+
+void turnOn(String deviceId) {
+  if (deviceId == "5e363a56168ca72bf7600790") // Device ID of first device
+  {
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+    //fillKettle();
+    kettleOn();
+
+  }
+  else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
+  {
+    Serial.print("Turn on device id: ");
+    Serial.println(deviceId);
+  }
+  else {
+    Serial.print("Turn on for unknown device id: ");
+    Serial.println(deviceId);
+  }
+}
+
+void turnOff(String deviceId) {
+   if (deviceId == "5e363a56168ca72bf7600790") // Device ID of first device
+   {
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+     kettleOff();
+   }
+   else if (deviceId == "5axxxxxxxxxxxxxxxxxxx") // Device ID of second device
+   {
+     Serial.print("Turn off Device ID: ");
+     Serial.println(deviceId);
+  }
+  else {
+     Serial.print("Turn off for unknown device id: ");
+     Serial.println(deviceId);
+  }
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+  switch(type) {
+    case WStype_DISCONNECTED:
+      isConnected = false;
+      Serial.printf("[WSc] Webservice disconnected from sinric.com!\n");
+      break;
+    case WStype_CONNECTED: {
+      isConnected = true;
+      Serial.printf("[WSc] Service connected to sinric.com at url: %s\n", payload);
+      Serial.printf("Waiting for commands from sinric.com ...\n");
+      }
+      break;
+    case WStype_TEXT: {
+        Serial.printf("[WSc] get text: %s\n", payload);
+        // Example payloads
+
+        // For Switch or Light device types
+        // {"deviceId": xxxx, "action": "setPowerState", value: "ON"} // https://developer.amazon.com/docs/device-apis/alexa-powercontroller.html
+
+        // For Light device type
+        // Look at the light example in github
+#if ARDUINOJSON_VERSION_MAJOR == 5
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject((char*)payload);
+#endif
+#if ARDUINOJSON_VERSION_MAJOR == 6
+        DynamicJsonDocument json(1024);
+        deserializeJson(json, (char*) payload);
+#endif
+        String deviceId = json ["deviceId"];
+        String action = json ["action"];
+
+        if(action == "setPowerState") { // Switch or Light
+            String value = json ["value"];
+            if(value == "ON") {
+                turnOn(deviceId);
+            } else {
+                turnOff(deviceId);
+            }
+        }
+        else if (action == "SetTargetTemperature") {
+            String deviceId = json ["deviceId"];
+            String action = json ["action"];
+            String value = json ["value"];
+        }
+        else if (action == "test") {
+            Serial.println("[WSc] received test command from sinric.com");
+        }
+      }
+      break;
+    case WStype_BIN:
+      Serial.printf("[WSc] get binary length: %u\n", length);
+      break;
+  }
+}
+
+void fillKettle() {
+    int x;
+    digitalWrite(5, ON);
+
+    delay(FILL_DELAY * 1000);  // waits FILL_DELAY for the kettle to fill
+    digitalWrite(5, OFF);
+
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFiMulti.addAP(MySSID, MyWifiPassword);
+  Serial.println();
+  Serial.print("Connecting to Wifi: ");
+  Serial.println(MySSID);
+
+  myservo.attach(2);  // attaches the servo on GIO2 to the servo object
+  myservo.write(90);              // tell servo to go to position in variable 'pos'
+
+  pinMode(5, OUTPUT);
+  digitalWrite(5, OFF);
+
+  // Waiting for Wifi connect
+  while(WiFiMulti.run() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  if(WiFiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.print("WiFi connected. ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
+
+  // server address, port and URL
+  webSocket.begin("iot.sinric.com", 80, "/");
+
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+  webSocket.setAuthorization("apikey", MyApiKey);
+
+  // try again every 5000ms if connection has failed
+  webSocket.setReconnectInterval(5000);   // If you see 'class WebSocketsClient' has no member named 'setReconnectInterval' error update arduinoWebSockets
+}
+
+void kettleOn() {
+  int pos;
+
+  // Turn kettle switch On
+  for (pos = 90; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+
+  // Back off the switch to neutral
+  for (pos = 120; pos >= 90; pos -= 1) { // goes from 180 degrees to 10 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void kettleOff() {
+  int pos;
+
+  // Turn kettle switch off
+  for (pos = 90; pos >= 30; pos -= 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+    // Turn kettle switch neutral
+  for (pos = 30; pos <= 90; pos += 1) { // goes from 180 degrees to 0 degrees
+    myservo.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(15);                       // waits 15ms for the servo to reach the position
+  }
+}
+
+void loop() {
+  webSocket.loop();
+
+  if(isConnected) {
+      uint64_t now = millis();
+
+      // Send heartbeat in order to avoid disconnections during ISP resetting IPs over night. Thanks @MacSass
+      if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
+          heartbeatTimestamp = now;
+          webSocket.sendTXT("H");
+      }
+  }
+}
+
+// If you want a push button: https://github.com/kakopappa/sinric/blob/master/arduino_examples/switch_with_push_button.ino
